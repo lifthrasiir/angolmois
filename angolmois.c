b6a6b63f5422178724b1ced9017970c9f97a4e06
Kang Seonghoon <public+hg@mearie.org>
2005-04-02 00:00:00 +0900

    2005-04-02 development version: * 글꼴 확대 출력 함수 추가. 매끈하게 보이려고 좀 삽질했음.
    --HG--
    branch : 1.0

--- angolmois.c
+++ angolmois.c
@@ -82,9 +82,9 @@ double length;
 #define ADD_STOP(time, index) add_note(21, (time), 0, (index))
 #define ADD_STP(time, index) add_note(22, (time), 0, (index))
 
-int isspace(char n) { return n==8 || n==10 || n==13 || n==32; }
-int getdigit(char n) { return 47<n && n<58 ? n-48 : ((n|32)-19)/26==3 ? (n|32)-87 : -1296; }
-int key2index(char a, char b) { return getdigit(a) * 36 + getdigit(b); }
+int isspace(int n) { return n==9 || n==10 || n==13 || n==32; }
+int getdigit(int n) { return 47<n && n<58 ? n-48 : ((n|32)-19)/26==3 ? (n|32)-87 : -1296; }
+int key2index(int a, int b) { return getdigit(a) * 36 + getdigit(b); }
 
 char *adjust_path(char *path) {
 	int i, j=0;
@@ -363,23 +363,23 @@ SDL_Surface *screen;
 SDL_Event event;
 
 /* compressed font data (whitespaces are ignored) */
-Uint8 fontmap[]="\0\2\3\6\7\10\13\14\16\20\30\33\34\36$,03678;<>?@AFU]_`acfghkl"
-	"nopsvwx{|~\177\201\237\303\333\361";
-Uint8 fontinfo[]=">',\37==8==M\\\256\211\255K==========MNM{M==================>"
-	"==========K=\26\315&]=]=]=_-=?==]]]``]]=]]]]_]-\20-7";
-Uint8 fontdata[]=".::::..$...66662'67;O7;O64));IIH;*III;))CDE'+.4F?'8JJJ9IKFK9+\
-    ++.-+.444444.+4.++++++.4.YYT.TYY....T......4T...%'+.4C=;EEGLRNEE;0:3++++++<\
-    ;EEG,08MCU;EE&1&&EE;,16FFFFU''UCCCT&&EE;;EECTEEEE;U&&'+.....;EEE;EEEE;;EEEE\
-    <&EE;...$$$......$$$...4'+.4C4.+'T$$TC4.+'+.4C;EEE'+.$..;>A@@@B>E;)06EEEUEE\
-    ETEEETEEEET15ECCCCE51SFEEEEEEFSUCCCTCCCCUUCCCTCCCCC15ECCGEE51EEEEUEEEEET...\
-    .....T&&&&&&EEE;EFJQMMQJFECCCCCCCCCUEPUUIIEEEEEEENRLGEEE06EEEEEE60TEEEETCCC\
-    C;EEEEEIL;(&TEEEETEEEE;EEC;&&EE;T.........EEEEEEEEE;EEEEEEE60)EEEEIIIU66>EP\
-    ;00;PE>VXF:......U&&(,08MCU:44444444:=C4.+'%:++++++++:)06E>U4.+';&&<EEE<CCC\
-    TEEEEET;EECCEE;&&&<EEEEE<;EEEUCE;,//.T.....<EEEE<&EE;CCCTEEEEEEE..$.......&\
-	&$$&&&&EEE;CCEFJQQJFE0+++++++++OUIIIIEETEEEEEEE;EEEEEE;TEEEEEETCCC<EEEEEE<&\
-	&&TEECCCCC;EC;&&E;..T...///,EEEEEEE;EEEEE60)EEEIIIU6>E6006E>EEEE51+.4CU(,08\
-	MCU,....M....,................M....,....MMZW,";
-int fontindex[96]={0};
+Uint8 fontmap[]="\0\2\3\6\7\10\13\14\16\20\30\33\34\36$,03678;<>?@ACU]^`acfghkl"
+	"nopsvwx{|~\177\201\202\237\303\333\371\376";
+Uint8 fontinfo[]="\37>',\37==8==M\\\256\211\255K==========MNM{M================"
+	"==>==========K=\26\315&]=]=]=_-=?==]]]``]]=]]]]_]-\20-7";
+Uint8 fontdata[]="\\WWWWWWWWWWWW\\.::::..$...66662'67;O7;O64));IIH;*III;))CDE'+\
+	.4E?&8JJJ9IKFK9+++.-+.444444.+4.++++++.4.ZZT.TZZ....T......4T...%'+.4C=;EEG\
+	LRNEE;0:3++++++<;EEG,08MCU;EE&1&&EE;,16FFFFU''UCCCT&&EE;;EECTEEEE;U&&'+....\
+	.;EEE;EEEE;;EEEE<&EE;...$$$......$$$...4'+.4C4.+'T$$TC4.+'+.4C;EEE'+.$..;>A\
+	@@@@B=;)06EEEUEEETEEETEEEET15ECCCCE51SFEEEEEEFSUCCCTCCCCUUCCCTCCCCC15ECCGEE\
+	51EEEEUEEEEET........T&&&&&&EEE;EFJQMMQJFECCCCCCCCCUEPUUIIEEEEEEENRLGEEE06E\
+	EEEEE60TEEEETCCCC;EEEEEIL;(&TEEEETEEEE;EEC;&&EE;T.........EEEEEEEEE;EEEEEEE\
+	60)EEEEIIIU66>EP;00;PE>VYF:......U&&(,08MCU:44444444:=C4.+'%:++++++++:)06E>\
+	U4.+';&&<EEE<CCCTEEEEET;EECCEE;&&&<EEEEE<;EEEUCE;,//.T.....<EEEE<&EE;CCCTEE\
+	EEEEE..$.......&&$$&&&&EEE;CCEFJQQJFE0+++++++++OUIIIIEETEEEEEEE;EEEEEE;TEEE\
+	EEETCCC<EEEEEE<&&&TEECCCCC;EC;&&E;..T...///,EEEEEEE;EEEEE60)EEEIIIU6>E6006E\
+	>EEEE51+.4CU(,08MCU,....M....,................M....,....MM[X,";
+int rawfont[95][16]={0,};
 
 void putpixel(int x, int y, int c) { ((Uint32*)screen->pixels)[x+y*800]=c; }
 void drawhline(int x1, int x2, int y, int c) { while(x1<x2) putpixel(x1++, y, c); }
@@ -388,35 +388,59 @@ int blend(int x, int y, int a, int b) { int i=0;for(;i<24;i+=8)y+=((x>>i&255)-(y
 void putblendedpixel(int x, int y, int c, int o) { putpixel(x, y, blend(((Uint32*)screen->pixels)[x+y*800], c, o, 255)); }
 
 void fontprocess() {
-	int i,j=0;
-	for(i=0; i<95; i++) {
-		fontinfo[i]--;
-		fontindex[i+1] = fontindex[i] + fontinfo[i]%16 - fontinfo[i]/16 + 1;
-	}
-	for(i=0; i<fontindex[95]; i++) {
-		if(fontdata[i+j] == 32) i--,j++;
-		else fontdata[i] = fontmap[fontdata[i+j]-36];
-	}
+	int i,j,k=0;
+	for(i=0; i<95; i++)
+		for(j=--fontinfo[i]/16; j<=fontinfo[i]%16; k++)
+			if(!isspace(fontdata[k])) rawfont[i][j++] = fontmap[fontdata[k]-36];
 }
 
-void printchar(int x, int y, char c) {
-	int i, j, k;
-	if(c < 33 || c > 126) return;
-	for(i=fontindex[c-33],j=fontinfo[c-33]>>4; j<=(fontinfo[c-33]&15); i++,j++) {
-		for(k=0; k<8; k++) {
-			if(fontdata[i]&(1<<(7-k)))
-				putpixel(x+k, y+j, blend(0xFF80FF, 0xFFFFFF, j, 15));
+void printchar(int x, int y, int c, int u, int v) {
+	int i, j;
+	if(isspace(c)) return;
+	c -= (c<33 || c>126 ? c : 32);
+	for(i=0; i<16; i++)
+		for(j=0; j<8; j++)
+			if(rawfont[c][i]&(1<<(7-j)))
+				putpixel(x+j, y+i, blend(u, v, i, 15));
+}
+
+void _printzchar(int a, int b, int x, int y, int c) {
+	while(a<b) putpixel(x+a++, y, c);
+}
+
+void printzchar(int x, int y, int z, int c, int u, int v) {
+	int i, j, k, m, n;
+	if(isspace(c)) return;
+	c -= (c<33 || c>126 ? c : 32);
+	for(i=0; i<16; i++,y+=z)
+		for(j=0; j<8; j++) {
+			m = (i>0?rawfont[c][i-1]<<j>>6&7:0)<<6 |
+				(rawfont[c][i]<<j>>6&7)<<3 |
+				(i<15?rawfont[c][i+1]<<j>>6&7:0);
+			if((c==3 || c==20) && j<2) m |= (m & 0222) << 1;
+			if((c==3 || c==20) && j>6) m |= (m & 0222) >> 1;
+			for(k=0; k<z; k++) {
+				n = blend(u, v, i*z+k, 16*z-1);
+				if(m & 0x10) {
+					_printzchar(0,z,x+j*z,y+k,n);
+				} else {
+					if((m & 0xda) == 0xa || (m & 0x3f) == 0xb) /* /| */
+						_printzchar(z-k,z,x+j*z,y+k,n);
+					if((m & 0x1b2) == 0x22 || (m & 0x3f) == 0x26) /* |\ */
+						_printzchar(0,k,x+j*z,y+k,n);
+					if((m & 0x9b) == 0x88 || (m & 0x1f8) == 0xc8) /* \| */
+						_printzchar(k+1,z,x+j*z,y+k,n);
+					if((m & 0xb6) == 0xa0 || (m & 0x1f8) == 0x1a0) /* |/ */
+						_printzchar(0,z-k-1,x+j*z,y+k,n);
+				}
+			}
 		}
-	}
 }
 
-void printstr(int x, int y, char *s) {
-	while(*s) {
-		printchar(x, y, *s++);
-		x += 8;
-	}
-}
+void printstr(int x, int y, char *s, int u, int v) { for(;*s;x+=8)printchar(x,y,(Uint8)*s++,u,v); }
+void printzstr(int x, int y, int z, char *s, int u, int v) { for(;*s;x+=8*z)printzchar(x,y,z,*s++,u,v); }
 
+#if 0
 int test() {
 	int i, j;
 	/* test section */
@@ -447,6 +471,7 @@ int test() {
 
 	return 0;
 }
+#endif
 
 /*
 angolmois
@@ -462,7 +487,7 @@ angolmois <filename> <options...>
 note: ranking file is <filename>.arank
 */
 int main(int argc, char **argv) {
-	int i;
+	int i, j, k, q;
 
 	if(SDL_Init(SDL_INIT_VIDEO|SDL_INIT_AUDIO)<0) return 1;
 	atexit(SDL_Quit);
@@ -471,23 +496,25 @@ int main(int argc, char **argv) {
 
 	fontprocess();
 	SDL_WM_SetCaption("TokigunStudio Angolmois: development version", "angolmois-dev (2005/03/31)");
-	printstr(20, 20, "TokigunStudio Angolmois (alpha version!)");
-	printstr(20, 40, "3.1415926535897932384662643383279............");
+	printzstr(20, 4, 2, "\1 TokigunStudio Angolmois (alpha version!)", 0x80FF80, 0xFFFFFF);
+	printstr(20, 40, "3.1415926535897932384662643383279............", 0xFF80FF, 0xFFFFFF);
 	for(i=0; i<95; i++)
-		printchar(20+i*8, 60, i+33);
+		printchar(20+i*8, 60, i+32, 0xFFFF80, 0xFFFFFF);
 	SDL_UpdateRect(screen, 0, 0, 800, 600);
 	
-	i = 1;
-	while(i) {
-		/*
+	q = 1;
+	while(q) {
 		if(!SDL_MUSTLOCK(screen) || SDL_LockSurface(screen)>=0) {
-			drawhline(0, 800, 300, (SDL_GetTicks() & 0xFF) * 0x10101);
+			for(i=500; i<550; i++) drawhline(0, 800, i, 0x000000);
+			while(k++<SDL_GetTicks()) printf("*");
+			printf("\n");
+			i = 600; j = k = SDL_GetTicks();
+			while(j) { printzchar(i-=24, 500, 3, "0123456789ABCDEF"[j%16], 0x808080, 0xFFFFFF); j/=16; }
 			SDL_UnlockSurface(screen);
 		}
 		SDL_UpdateRect(screen, 0, 0, 800, 600);
-		*/
 		while(SDL_PollEvent(&event)) {
-			if(event.type==SDL_QUIT || (event.type==SDL_KEYUP && event.key.keysym.sym==SDLK_ESCAPE)) i = 0;
+			if(event.type==SDL_QUIT || (event.type==SDL_KEYUP && event.key.keysym.sym==SDLK_ESCAPE)) q = 0;
 		}
 	}
 	
